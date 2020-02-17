ba7a18eba744bea34e6cddd5905b9b8491e0e576
scofieldpeng <penjianfeng@gmail.com>
2020-02-17 11:06:33 +0800

    update getrundir

--- filepath/filepath.go
+++ filepath/filepath.go
@@ -1,16 +1,25 @@
 package filepath
 
 import (
-	"path/filepath"
 	"os"
+	"path/filepath"
 )
 
-// 获取程序运行时的当前目录
-func GetRunDir() (path string, err error) {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+// 获取当前二进制文件运行时的当前目录
+func GetRunDir() (dir string, err error) {
+	filePath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	dir, err = filepath.Abs(filepath.Dir(filePath))
 	if err != nil {
 		return "", err
 	}
 
 	return dir, nil
+}
+
+// 获取当前所在的目录，这个建议开发时使用，尤其是go run这种
+func GetCurrentPwd() (dir string, err error){
+	return os.Getwd()
 }
